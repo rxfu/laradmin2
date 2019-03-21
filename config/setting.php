c626f142be0ca16913828fafacdf147b787ce933
Stephen Fu <stephen.frx@gmail.com>
2019-03-22 00:30:10 +0800

    add predis

---
+++ config/setting.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'base' => '/laradmin',
+    'name' => 'Laravel Admin',
+    'slug' => 'LA',
+    'keywords' => 'Laravel, Vue.js, Bootstrap, Admin page',
+    'description' => 'Administration boilerplate using Laravel',
+    'author' => 'Fu Rongxin',
+    'copyright' => 'Fu Rongxin'
+];
