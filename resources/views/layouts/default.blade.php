4fafb122ec8a24c62f14a8fb80d12049cf288ab6
Stephen Fu <stephen.frx@gmail.com>
2019-03-22 13:36:05 +0800

    add layouts

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="keywords" content="{{ config('setting.keywords') }}">
+    <meta name="description" content="{{ config('setting.description') }}">
+    <meta name="author" content="{{ config('setting.author') }}">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title', '默认页面') | {{ config('setting.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <!-- Font Awesome Icons -->
+    <link href="{{ asset('vendor/font-awesome/css/all.min.css') }}" rel="stylesheet">
+    <!-- Theme style -->
+    <link href="{{ asset('css/adminlte.min.css') }}" rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Custom styles -->
+    @stack('styles')
+</head>
+<body class="hold-transition @yield('body-class')">
+
+    @yield('page')
+    
+    <!-- jQuery -->
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!-- AdminLTE App -->
+    <script src="{{ asset('js/adminlte.min.js') }}"></script>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Custom scripts ==>
+    @stack('scripts')
+</body>
+</html>
---
+++ resources/views/shared/sidebar.blade.php
@@ -0,0 +1,102 @@
+<!-- Main sidebar -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  	<!-- Brand logo -->
+  	<a href="{{ url('/') }}" class="brand-link">
+		<img src="{{ asset('img/AdminLTELogo.png') }}" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+		<span class="brand-text font-weight-light">{{ config('setting.slug') }}</span>
+  	</a>
+
+  	<!-- Sidebar -->
+  	<div class="sidebar">
+      	<!-- Sidebar user panel (optional) -->
+      	<div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          	<div class="image">
+            	<img src="{{ asset('img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="用户头像">
+          	</div>
+          	<div class="info">
+            	<a href="#" class="d-block">{{ $name }}</a>
+          	</div>
+        </div>
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          	    <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
+                @foreach (config('menu.sidebar.' . $guard) as $items)
+                    @foreach ($items as $item)
+                        @if (is_array($item))
+                            @isset($item['children'])
+                                <li class="nav-item has-treeview">
+                                    <a href="
+                                    @isset($item['route'])
+                                        {{ route($item['route']) }}
+                                    @else
+                                        @isset ($item['url'])
+                                            {{ url($item['url']) }}
+                                        @else
+                                            #
+                                        @endisset
+                                    @endisset
+                                    " class="nav-link">
+                                        @isset ($item['icon'])
+                                            <i class="nav-icon fa fa-{{ $item['icon'] }}"></i>
+                                        @endisset
+                                        <p>
+                                            {{ $item['title'] ?? '无标题' }}
+                                            <i class="right fa fa-angle-left"></i>
+                                        </p>
+                                    </a>
+                                    <ul class="nav nav-treeview">
+                                        @foreach ($item['children'] as $subitem)
+                                            <li class="nav-item">
+                                                <a href="
+                                                @isset($subitem['route'])
+                                                    {{ route($subitem['route']) }}
+                                                @else
+                                                    @isset ($subitem['url'])
+                                                        {{ url($subitem['url']) }}
+                                                    @else
+                                                        #
+                                                    @endisset
+                                                @endisset
+                                                " class="nav-link">
+                                                    @isset ($subitem['icon'])                                          
+                                                        <i class="nav-icon fa fa-{{ $subitem['icon'] }}"></i>
+                                                    @else
+                                                        <i class="nav-icon fa fa-circle-o"></i>
+                                                    @endisset
+                                                    <p>{{ $subitem['title'] ?? '无标题' }}</p>
+                                                </a>
+                                            </li>
+                                        @endforeach
+                                    </ul>
+                                </li>
+                            @else
+                                <li class="nav-item">
+                                    <a href="
+                                    @isset($item['route'])
+                                        {{ route($item['route']) }}
+                                    @else
+                                        @isset ($item['url'])
+                                            {{ url($item['url']) }}
+                                        @else
+                                            #
+                                        @endisset
+                                    @endisset
+                                    " class="nav-link">
+                                        @isset ($item['icon'])
+                                            <i class="nav-icon fa fa-{{ $item['icon'] }}"></i>
+                                        @endisset
+                                        <p>{{ $item['title'] ?? '无标题' }}</p>
+                                    </a>
+                                </li>
+                            @endisset
+                        @else
+                            <li class="nav-header">{{ $item }}</li>
+                        @endif
+                    @endforeach
+                @endforeach
+            </ul>
+        </nav>
+  	</div>
+</aside>
